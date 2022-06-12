bcb776dbd6d4bc4f6591c9dab4646bf3af0cbe9b
Fernando Martinez Jr <fernando.martinez.jr.59@gmail.com>
2022-06-12 12:11:04 -0400

    testing heroku

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -41,6 +41,8 @@ router.post('/login', async (req, res) => {
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
+      req.session.name = userData.username;
+      // req.
 
       res.json({ user: userData, message: 'You are now logged in!' });
     });
